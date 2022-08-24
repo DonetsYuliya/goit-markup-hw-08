06b8c0486dc627a7136e4f3e1278ece0ef988621
DonetsYuliya <donecjuli@gmail.com>
2022-08-24 12:57:20 +0300

    created mobile first and tablet header

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,7 +1,7 @@
 (() => {
-    const mobileMenu = document.querySelector('.js-menu-container');
-    const openMenuBtn = document.querySelector('.js-open-menu');
-    const closeMenuBtn = document.querySelector('.js-close-menu');
+    const mobileMenu = document.querySelector('[js-menu-container]');
+    const openMenuBtn = document.querySelector('[js-open-menu]');
+    const closeMenuBtn = document.querySelector('[js-close-menu]');
     const toggleMenu = () => {
         const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
         openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
@@ -12,7 +12,6 @@
     openMenuBtn.addEventListener('click', toggleMenu);
     closeMenuBtn.addEventListener('click', toggleMenu);
 
-    // Close the mobile menu on wider screens if the device orientation changes
     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
         if (!e.matches) return;
         mobileMenu.classList.remove('is-open');
